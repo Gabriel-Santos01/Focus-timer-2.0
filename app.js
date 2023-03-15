665e8a747c3e22cbc93927d76441fe6e1674d9db
Gabriel-Santos01 <gabriel.adv45@outlook.com>
2023-03-15 14:19:05 -0300

    feat: functional timer

--- app.js
+++ app.js
@@ -7,19 +7,70 @@ const min = document.querySelector('.minutes')
 const sec = document.querySelector('.seconds')
 
 play.addEventListener('click', () => {
-  play.classList.toggle('hidden')
-  stop.classList.toggle('hidden')
+  playPause()
+  countdown()
 })
 
 stop.addEventListener('click', () => {
-  play.classList.toggle('hidden')
-  stop.classList.toggle('hidden')
+  reset()
+  clearTimeout(timerTimeOut)
+  clearDisplay()
+})
+
+pause.addEventListener('click', () => {
+  playPause()
+  clearTimeout(timerTimeOut)
 })
 
 plus.addEventListener('click', () => {
-  min.innerHTML = parseInt(min.innerHTML) + 5
+  timeSeted += 5
+  min.innerHTML = String(timeSeted).padStart(2, '0')
 })
 
 minus.addEventListener('click', () => {
-  min.innerHTML = parseInt(min.innerHTML) - 5
+  timeSeted -= 5
+  min.innerHTML = String(timeSeted).padStart(2, '0')
 })
+
+let timeSeted = 0
+let timerTimeOut
+
+function countdown() {
+  timerTimeOut = setTimeout(function () {
+    let minutes = Number(min.innerHTML)
+    let seconds = Number(sec.innerHTML)
+
+    sec.textContent = String(seconds - 1).padStart(2, '0')
+
+    if (minutes <= 0 && seconds == 1) {
+      playPause()
+
+      return
+    }
+
+    if (seconds <= 0) {
+      seconds = 59
+
+      min.textContent = String(minutes - 1).padStart(2, '0')
+    }
+
+    sec.textContent = String(seconds - 1).padStart(2, '0')
+
+    countdown()
+  }, 10)
+}
+
+function playPause() {
+  play.classList.toggle('hidden')
+  stop.classList.toggle('hidden')
+}
+
+function reset() {
+  play.classList.toggle('hidden')
+  stop.classList.toggle('hidden')
+}
+
+function clearDisplay() {
+  min.innerHTML = String(timeSeted).padStart(2, '0')
+  sec.innerHTML = '00'
+}
